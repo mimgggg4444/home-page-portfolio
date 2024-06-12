8c3508b79b6e9ae5c9f510e661ad647db304672f
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-06-12 15:10:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,18 +15,16 @@ function dateArea(){
     var hoursArea = new Date().getHours();
     var minutesArea = new Date().getMinutes();
     var clockArea = hoursArea + ":" + minutesArea;
-    
-    if(hoursArea<10){
-      var clockArea = "0" + hoursArea + ":" + minutesArea;
-    } else if(minutesArea<10){
-      var clockArea = hoursArea + ":0" + minutesArea;
-    } else{
+      
+      if(hoursArea<10){
+          hoursArea = "0" + hoursArea;
+      }
+      if(minutesArea<10){
+          minutesArea = "0" + minutesArea;
+      }
       var clockArea = hoursArea + ":" + minutesArea;
-    }
-    document.querySelector(".clock-area").innerHTML = clockArea;
-  }
-  
-  setInterval(clockArea, 1000);
+      document.querySelector(".clock-area").innerHTML = clockArea;
+      setInterval(clockArea, 1000);
   
   var ntf = document.querySelector(".bell");
   var ntfBell = document.querySelector(".sound");
